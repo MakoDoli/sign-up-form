efe0cb4e63b0bf9609f0d67a5d3cd7dfd61a7d7f
MakoDoli <makodolidze@hotmail.com>
2023-05-03 21:14:47 +0400

    sign-up form

---
+++ src/components/Form.js
@@ -0,0 +1,112 @@
+import "./Form.css";
+import { useNavigate } from "react-router-dom";
+import { useState, useReducer } from "react";
+
+const userArray = [];
+
+export let newUser = {
+  name: "",
+  surname: "",
+  email: "",
+  password: "",
+};
+export default function Form() {
+  const [userEmail, setUserEmail] = useState("");
+
+  const [objData, setObjData] = useState(newUser);
+
+  const reducer = function (state, action) {
+    switch (action.type) {
+      case "UPDATE_NAME":
+        return { ...state, name: action.payload };
+      case "UPDATE_LASTNAME":
+        return { ...state, surname: action.payload };
+      case "UPDATE-EMAIL":
+        return { ...state, email: action.payload };
+      case "UPDATE_PASSWORD":
+        return { ...state, password: action.payload };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, newUser);
+
+  const navigate = useNavigate();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //რამე ფუნქცია რითაც შეყვანილ მონაცემებს/ობიექტს რამე უნდა ვუქნათ. მაგ., გავuშვათ API request ან ბექში (ეგ ჯერ არ ვიცი)
+    state.email = userEmail;
+    console.log(state);
+    // setObjData(state);
+    newUser = state;
+    console.log(newUser);
+    userArray.push(state);
+    //console.log(userArray);
+    navigate("/return");
+  };
+
+  return (
+    <div className="form">
+      <div className="heading">
+        <h1>Learn to code by watching others</h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.{" "}
+        </p>
+      </div>
+      <div className="form-container">
+        <div className="price">
+          <p>
+            <span className="try-free">Try it free 7 days</span> then $20/mo.
+            thereafter
+          </p>
+        </div>
+        <div className="form-inputs">
+          <form onSubmit={submitHandler}>
+            <label htmlFor="name"></label>
+            <input
+              id="name"
+              type="text"
+              onChange={(e) =>
+                dispatch({ type: "UPDATE_NAME", payload: e.target.value })
+              }
+              placeholder="Your name"
+              value={state.name}
+            />
+
+            <input
+              placeholder="Last name"
+              type="text"
+              onChange={(e) =>
+                dispatch({ type: "UPDATE_LASTNAME", payload: e.target.value })
+              }
+              value={state.surname}
+            />
+            <input
+              placeholder="Email"
+              type="email"
+              onChange={(e) => setUserEmail(e.target.value)}
+              value={userEmail}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              onChange={(e) =>
+                dispatch({ type: "UPDATE_PASSWORD", payload: e.target.value })
+              }
+            />
+
+            <button type="sumbit">CLAIM YOUR FREE TRIAL</button>
+
+            <p>
+              By clicking the button, you are agreeing to our{" "}
+              <span className="terms">Terms and Services</span>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Return/Return.js
@@ -0,0 +1,67 @@
+import { Link, useNavigate } from "react-router-dom";
+import { newUser } from "../../components/Form";
+import "../../components/Form.css";
+import { useState } from "react";
+import { setObjData } from "../../components/Form";
+
+export default function Return() {
+  const navigate = useNavigate();
+
+  const reset = () => {
+    navigate("/");
+    setObjData("");
+  };
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="form">
+      <div className="form-container">
+        <div className="price" style={{ paddingTop: 0 }}>
+          <p>
+            <span className="try-free">Dear {newUser.name.toUpperCase()}</span>{" "}
+            <br></br>Thank you for signing up
+          </p>
+        </div>
+        <div className="form-inputs">
+          <form onSubmit={reset}>
+            <label htmlFor="name"></label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Your name"
+              defaultValue={newUser.name}
+            />
+
+            <input
+              placeholder="Last name"
+              type="text"
+              defaultValue={newUser.surname}
+            />
+            <input
+              placeholder="Email"
+              type="email"
+              defaultValue={newUser.email}
+            />
+            <div className="password-section">
+              <input
+                className="password-input"
+                placeholder="Password"
+                type={visible ? "text" : "password"}
+                defaultValue={newUser.password}
+              />
+              <div onClick={() => setVisible(!visible)} className="show">
+                show
+              </div>
+            </div>
+
+            <button onClick={reset}>click here to return</button>
+
+            <p>
+              By clicking the button, you are agreeing to our{" "}
+              <span className="terms">Terms and Services</span>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
